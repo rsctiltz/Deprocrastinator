4c056bb7d5cdac86f4e3b284340b974c676d2e51
rsctiltz <rsctiltz@gmail.com>
2014-05-19 14:29:54 -0500

    Step 1

---
+++ Deprocrastinator/Deprocrastinator/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  Deprocrastinator
+//
+//  Created by Ryan Tiltz on 5/19/14.
+//  Copyright (c) 2014 Ryan Tiltz. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
